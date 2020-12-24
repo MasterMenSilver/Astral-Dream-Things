62714c894b250d36c5b6e722775e886b5a200f32
MasterMen <38983673+MasterMenOfficial@users.noreply.github.com>
2020-12-24 09:49:51 +0500

    Update CmdStaff.cs

--- CmdStaff.cs
+++ CmdStaff.cs
@@ -1,14 +1,16 @@
 using System;
 
+// -------------------------- //
+// Made by MasterMen for Astral Dream
+// -------------------------- //
 // Written in: 2020.11.XX
-// Last Update: 2020.11.26
+// Last Update: 2020.12.24
+// -------------------------- //
 
 namespace MCGalaxy {
 	
 	public class CmdStaff : Command2 {
 		public override string name { get { return "Staff"; } }
-		//public override string MCGalaxy_Version { get { return "1.9.2.7"; } }
-		//public override string creator { get { return "MasterMen"; } }
 		public override string shortcut { get { return "st"; } }
 		public override bool MessageBlockRestricted { get { return false; } }
 		public override string type { get { return "other"; } }
@@ -20,9 +22,10 @@ namespace MCGalaxy {
 			Command.Find("viewranks").Use(p, "Moderator");
 			Command.Find("viewranks").Use(p, "Trusted");
 		}
+		
 		public override void Help(Player p) {
 			p.Message("%T/Staff");
-            p.Message("%HShows you the staff members.");
+            		p.Message("%HShows you the staff members.");
 		}
 	}
 	
