75f85dd4c66241c81b93dab0ab9a9c6efc5ef1b7
MasterMen <38983673+MasterMenOfficial@users.noreply.github.com>
2020-12-24 09:33:31 +0500

    Updated to a final version with the player check.

--- CmdGIMP.cs
+++ CmdGIMP.cs
@@ -3,32 +3,35 @@ using MCGalaxy;
 using MCGalaxy.Commands;
 using MCGalaxy.DB;
 
+// -------------------------- //
+// Made by MasterMen for Astral Dream.
+// -------------------------- //
 // Written in: 2020.11.14
-// Last Update: 2020.11.26
+// Last Update: 2020.12.24
+// -------------------------- //
 
 namespace MCGalaxy {
 
 	public class CmdGIMP : Command2 {
 		public override string name { get { return "GIMP"; } }
-		//public override string MCGalaxy_Version { get { return "1.9.2.7"; } }
-		//public override string creator { get { return "MasterMen"; } }
 		public override string shortcut { get { return "gimp"; } }
 		public override string type { get { return "other"; } }
 		public override bool museumUsable { get { return false; } }
 		public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
 		
 		public override void Use(Player p, string message, CommandData data) {
-			//if (message == "") { Help(p); return; }
-			//string shadows = PlayerInfo.FindMatchesPreferOnline(p, message);
-			//Player shadows = PlayerInfo.FindExact(name);
 			
-			//if (shadows == null) return;
+			//p.DisplayName
+			//p.truename
+			//p.name
+			//p.ColoredName
 			
-			//Player shadows = FindMatches(p, shadows);
-			//Player shadows = PlayerInfo.FindMatches(p, shadows);
-			//shadowsname = PlayerInfo.GetColoredName(p, shadows);
-			string shadows = "shadows";
-			Chat.MessageGlobal("{0}%7, You are now a %i{1} %7follower! Welcome to the %eGIMP %7gang!", p.ColoredName, shadows);
+			Player shad = PlayerInfo.FindMatches(p, "shadows");
+			if(p == shad) {
+				Chat.MessageGlobal("{0} is a GIMP king! Everybody do /GIMP command and become their slave!", p.ColoredName);
+			} else {
+				Chat.MessageGlobal("{0}%7, You are now a %i{1} %7follower! Welcome to the %eGIMP %7gang!", p.ColoredName, shad.ColoredName);
+			}
 		}
 		
 		public override void Help(Player p) {
